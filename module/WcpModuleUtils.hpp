c33be04d0bbdf72840642cd2a84baab7cabd4e19
Yurter <sterben_idd@mail.ru>
2019-08-06 11:31:25 +0300

    [Cleanup]

--- module/WcpModuleUtils.hpp
+++ module/WcpModuleUtils.hpp
@@ -14,111 +14,58 @@ class WcpModuleUtils
 
 public:
 
-    /* Метод принимает cv::Mat изображение, возврщает base64 */
-    static std::string encodeBase64Image(cv::Mat cvimg)
-    {
-        std::string ret;
-        int i = 0;
-        int j = 0;
-        unsigned char char_array_3[3];
-        unsigned char char_array_4[4];
-
-        uchar const* bytes_to_encode = cvimg.data;
-        int in_len = cvimg.cols * cvimg.rows;
-
-        while (in_len--) {
-            char_array_3[i++] = *(bytes_to_encode++);
-            if (i == 3) {
-                char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-                char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-                char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-                char_array_4[3] = char_array_3[2] & 0x3f;
-
-                for (i = 0; (i <4) ; i++) {
-                    ret += base64_chars[char_array_4[i]];
-                }
-                i = 0;
-            }
-        }
-
-        if (i) {
-            for(j = i; j < 3; j++) {
-                char_array_3[j] = '\0';
-            }
-
-            char_array_4[0] = ( char_array_3[0] & 0xfc) >> 2;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-
-            for (j = 0; (j < i + 1); j++) {
-                ret += base64_chars[char_array_4[j]];
-            }
-
-            while((i++ < 3)) {
-                ret += '=';
-            }
-        }
-
-        return ret;
-    }
-
-    /* Метод принимает base64 изображение из входного джейсона, возврщает cv::Mat */
-    static cv::Mat decodeBase64Image(std::string encoded_image, int width, int height)
-    {
-        std::cout << "decodeBase64Image - " <<  width << " " << height << std::endl;
-        size_t in_len = encoded_image.size();
-        int i = 0;
-        int j = 0;
-        int in_ = 0;
-        unsigned char char_array_4[4], char_array_3[3];
-        std::string decoded_string;
-
-        auto isBase64 = [](unsigned char c) { return (isalnum(c) || (c == '+') || (c == '/')); };
-
-        while (in_len-- && (uchar)(encoded_image[in_] != '=') && isBase64((uchar)encoded_image[in_])) {
-            char_array_4[i++] = (uchar)encoded_image[in_]; in_++;
-            if (i == 4) {
-                for (i = 0; i < 4; i++)
-                    char_array_4[i] = base64_chars.find(char_array_4[i]);
-
-                char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-                char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-                char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-                for (i = 0; (i < 3); i++)
-                    decoded_string += char_array_3[i];
-                i = 0;
-            }
-        }
-
-        if (i) {
-            for (j = i; j < 4; j++)
-                char_array_4[j] = 0;
-
-            for (j = 0; j < 4; j++)
-                char_array_4[j] = base64_chars.find(char_array_4[j]);
-
-            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-            for (j = 0; (j < i - 1); j++) decoded_string += char_array_3[j];
-        }
-
-//        std::cout << "decoded_string - " << decoded_string << std::endl;
-        std::vector<uchar> data(decoded_string.begin(), decoded_string.end());
-        cv::Mat cvimg = imdecode(data, cv::IMREAD_UNCHANGED);
-//        std::cout << "vector<ucgar> to mat: " << data.size() << std::endl;
-//        cv::Mat cvimg(width, height, 16/*CV_8UC1*/, data.data());
-//        std::cout << "\n\n\n\n\ncvimg - " << cvimg.cols * cvimg.rows << std::endl;
-        std::cout << "[DebugM] image size = " << cvimg.cols << "x"<< cvimg.rows << "=" << cvimg.cols * cvimg.rows << std::endl;
-        return cvimg;
-    }
-
     /* Метод проверяет наличие ключа в переданном джейсоне */
     static bool keyExist(const nlohmann::json& js, std::string key)
     {
         return js.find(key) != js.end();
+    }
+
+    /* Метод собирает джейсон из cv картинки */
+    static nlohmann::json imageToJson(cv::Mat& cvimage)
+    {
+        nlohmann::json jscvimage;
+
+        std::vector<uchar> data_array;
+        if (cvimage.isContinuous()) {
+            data_array.assign(const_cast<uchar*>(cvimage.datastart)
+                              , const_cast<uchar*>(cvimage.dataend));
+        } else {
+            for (int i = 0; i < cvimage.rows; ++i) {
+                data_array.insert(data_array.end()
+                                  , cvimage.ptr<uchar>(i)
+                                  , cvimage.ptr<uchar>(i) + cvimage.cols);
+            }
+        }
+
+//        jscvimage["image"]["width"] = cvimage.cols;
+//        jscvimage["image"]["height"] = cvimage.rows;
+//        jscvimage["image"]["type"] = cvimage.type();
+//        jscvimage["image"]["data"] = data_array;
+        jscvimage["width"] = cvimage.cols;
+        jscvimage["height"] = cvimage.rows;
+        jscvimage["type"] = cvimage.type();
+        jscvimage["data"] = data_array;
+
+        return jscvimage;
+    }
+
+    /* Метод собирает cv картинку из джейсона */
+    static cv::Mat jsonToImage(const nlohmann::json& jscvimage)
+    {
+//        std::vector<uchar> data = jscvimage["image"]["data"];
+
+//        cv::Mat cvimage(jscvimage["image"]["height"]
+//                , jscvimage["image"]["width"]
+//                , jscvimage["image"]["type"]
+//                , data.data());
+        std::vector<uchar> data = jscvimage["data"];
+
+        cv::Mat cvimage(jscvimage["height"]
+                , jscvimage["width"]
+                , jscvimage["type"]
+                , data.data());
+
+        return cvimage;
     }
 
 };
--- module_controller/WcpModuleController.cpp
+++ module_controller/WcpModuleController.cpp
@@ -14,66 +14,19 @@ bool WcpModuleController::initGraph()
 
 nlohmann::json WcpModuleController::propagateImage(cv::Mat image)
 {
-    cv::imwrite("Debug3.png", image);
+    std::cout << __FUNCTION__ << std::endl;
     nlohmann::json output_json;
     //temp solution
     nlohmann::json input_data;
     input_data["action"] = "detect";
-    if (WcpModuleUtils::keyExist(input_data, "action") == false) {
-        std::cout << "[Debug] action does not exist" << std::endl;
-        return nlohmann::json();
-    }
-    std::cout << "[Debug] input_data = " << input_data << std::endl;
-    std::cout << "[Debug] image type = " << image.type() << " " << CV_8UC1 << std::endl;
-    std::cout << "[Debug] image size = " << image.cols << "x"<< image.rows << "=" << image.cols * image.rows << std::endl;
-//    std::cout << "[Debug] image = " << image << std::endl;
-//    std::cout << "[Debug] WcpModuleUtils::encodeBase64Image(image) = " << WcpModuleUtils::encodeBase64Image(image) << std::endl;
-//    input_data["image"].push_back({
-//                                      { "width", image.cols }
-//                                      , { "height", image.rows }
-//                                      , { "data", WcpModuleUtils::encodeBase64Image(image)
-//                                      }
-//                                  });
+    input_data["image"] = WcpModuleUtils::imageToJson(image);
 
-    std::vector<uchar> array2;
-    if (image.isContinuous()) {
-      array2.assign((uchar*)image.datastart, (uchar*)image.dataend);
-    } else {
-      for (int i = 0; i < image.rows; ++i) {
-        array2.insert(array2.end(), image.ptr<uchar>(i), image.ptr<uchar>(i)+image.cols);
-      }
-    }
-
-    input_data["image"]["width"] = image.cols;
-    input_data["image"]["height"] = image.rows;
-//    input_data["image"]["data"] = WcpModuleUtils::encodeBase64Image(image);
-//    input_data["image"]["data"] = std::vector<uchar>(image.data, image.data + image.cols * image.rows);
-    input_data["image"]["type"] = image.type();
-    input_data["image"]["data"] = array2;
-
-
-
-
-
-
-    std::vector<uchar> test_arr = input_data["image"]["data"];
-
-    std::vector<uchar> test_data = input_data["image"]["data"];
-    cv::Mat source_cvimg(input_data["image"]["height"]
-            , input_data["image"]["width"]
-            , input_data["image"]["type"] //CV_8U
-            , test_arr.data());
-    cv::imwrite("Debug2.png", source_cvimg);
-//    return 0;
-
-
-    std::cout << "[Debug] std::vector<uchar>(image.data, image.data + image.cols * image.rows) = " << std::vector<uchar>(image.data, image.data + image.cols * image.rows).size() << std::endl;
-//    input_data["image"] = WcpModuleUtils::encodeBase64Image(image);
     for (auto&& module : _module_list) {
         auto ret = module->process(input_data.dump().c_str());
         auto module_answer = nlohmann::json::parse(ret);
         std::cout << "[Debug] module_answer = " << module_answer << std::endl;
         output_json.push_back({ module->name(), module_answer });
     }
+
     return output_json;
 }
