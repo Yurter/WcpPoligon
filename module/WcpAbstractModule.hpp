08de246f2b95bfc44f47974e58f77c5213a4aef2
Yurter <sterben_idd@mail.ru>
2019-08-09 15:31:52 +0300

    [Added] Методы чтения и записи модулем произвольных данных.

--- module/WcpAbstractModule.hpp
+++ module/WcpAbstractModule.hpp
@@ -66,8 +66,7 @@ public:
     void                setUsed(bool used) { _used = used; }
 
     /* Си интерфейс для метода process(const nlohmann::json input_data, nlohmann::json& output_data) : void */
-    [[nodiscard]] const char* process(const char* input_data)
-    {
+    [[nodiscard]] const char* process(const char* input_data) {
         nlohmann::json output_data;
         process(nlohmann::json::parse(input_data), output_data);
         _json_dump_buffer = output_data.dump();
@@ -90,25 +89,27 @@ protected:
      *                                ex. 2: [ "face" : face_array ]                                           Монообъектный детектор
      * }
     **/
-    void process(const nlohmann::json input_data, nlohmann::json& output_data)
-    {
+    void process(const nlohmann::json input_data, nlohmann::json& output_data) {
+
         /* Проверка наличия полей с определенными ключами */
         if (WcpModuleUtils::keyExist(input_data, "action") == false) {
             throw_exception("missing key \"action\" in input data");
         }
-        if (WcpModuleUtils::keyExist(input_data, "data_array") == false) {
-            throw_exception("missing key \"data_array\" in input data");
-        }
+
         /* Проверка типа полей */
         if (input_data["action"].is_string() == false) {
             throw_exception("value of \"action\" field is not a string");
         }
-        if (input_data["data_array"].is_array() == false) {
-            throw_exception("value of \"data_array\" field is not an array");
-        }
 
         /* При предусмотренном типе действия, происходит вызов соответствующего метода */
         if (input_data["action"] == "process") {
+            if (WcpModuleUtils::keyExist(input_data, "data_array") == false) {
+                throw_exception("missing key \"data_array\" in input data");
+            }
+            if (input_data["data_array"].is_array() == false) {
+                throw_exception("value of \"data_array\" field is not an array");
+            }
+
             nlohmann::json input_data_array = input_data["data_array"];
             nlohmann::json output_data_array;
             if (onProcess(input_data_array, output_data_array) == false) {
@@ -140,70 +141,62 @@ protected:
         }
 
         /* При непредусмотренном типе действия, происходит вызов универсального метода */
-        nlohmann::json input_data_array = input_data["data_array"];
-        nlohmann::json output_data_array;
-        if (onAction(input_data["action"], input_data_array, output_data_array) == false) {
-            output_data["status"] = "failed";
+        {
+            if (WcpModuleUtils::keyExist(input_data, "data_array") == false) {
+                throw_exception("missing key \"data_array\" in input data");
+            }
+            if (input_data["data_array"].is_array() == false) {
+                throw_exception("value of \"data_array\" field is not an array");
+            }
+
+            nlohmann::json input_data_array = input_data["data_array"];
+            nlohmann::json output_data_array;
+            if (onAction(input_data["action"], input_data_array, output_data_array) == false) {
+                output_data["status"] = "failed";
+                return;
+            }
+            /* Проверка ответа модуля */
+            if (output_data_array.is_array() == false) {
+                throw_exception("returned value is not an array");
+            }
+            output_data["status"] = "success";
+            output_data["data_array"] = output_data_array;
             return;
         }
-        /* Проверка ответа модуля */
-        if (output_data_array.is_array() == false) {
-            throw_exception("returned value is not an array");
-        }
-        output_data["status"] = "success";
-        output_data["data_array"] = output_data_array;
-        return;
     }
 
     virtual bool onProcess(const nlohmann::json input_data_array, nlohmann::json& output_data_array) = 0;
-    virtual bool onSetCallback(int64_t func_pointer)
-    {
+    virtual bool onSetCallback(int64_t func_pointer) {
         _callback_func = reinterpret_cast<CallbackFunc>(func_pointer);
         registerController();
+        loadData();
         return true;
     }
-    virtual bool onAction(const std::string action, const nlohmann::json input_data_array, nlohmann::json& output_data_array)
-    {
+    virtual bool onAction(const std::string action, const nlohmann::json input_data_array, nlohmann::json& output_data_array) {
         UNUSED(action) UNUSED(input_data_array) UNUSED(output_data_array)
         throw_exception(action + " action called, but virtual function \"onAction\" not overrided");
     }
 
-    /* Модуль может использовать постоянное внешнее хранилище данных, содержимое которого
-     * хранится в произваольной форме. Запись и чтение происходит со всей информацией разом */
-    void saveData(nlohmann::json data)
-    {
-        if (_callback_func == nullptr) {
-            throw_exception("Callback function is null");
-        }
-        nlohmann::json request;
-        nlohmann::json response;
-        request["module"] = _workname;
-        request["action"] = "save";
-        request["data"] = data;
-        _callback_func(request, response);
+    /* Метод возвращает данные, которые были сохранены ранее методом writeData */
+    nlohmann::json readData() {
+        return _data_buffer;
     }
-    nlohmann::json loadData()
-    {
-        if (_callback_func == nullptr) {
-            throw_exception("Callback function is null");
-        }
-        nlohmann::json request;
-        nlohmann::json response;
-        request["module"] = _workname;
-        request["action"] = "load";
-        _callback_func(request, response);
-        return response;
+
+    /* Метод заменяет данные, которые были сохранены ранее, новыми, переданными в качестве аргумента        */
+    /* Дозапись осуществляется путем вызова метода чтения, ручной модификацией данных, вызова метода записи */
+    void writeData(nlohmann::json jsupdated_data) {
+        _data_buffer = jsupdated_data;
+        saveData();
     }
 
     /* Метод вызывается внутри реализации onProcess при каждом успешном получении результатов */
-    void stashObject(std::string obj_name, nlohmann::json jsobj_value)
-    {
+    void stashObject(std::string obj_name, nlohmann::json jsobj_value) {
         /* Формирование json-объекта, поля котрого содержат массивы однотипных объектов */
         _stashed_objects[obj_name].push_back(jsobj_value);
     }
+
     /* Метод вызывается внутри реализации onProcess при завршении обработки входных данных и формировании ответа */
-    nlohmann::json dumpObjects()
-    {
+    nlohmann::json dumpObjects() {
         /* Преобразование полей json-объекта в массив */
         nlohmann::json jsdump;
         for (auto& item : _stashed_objects.items()) {
@@ -215,13 +208,40 @@ protected:
 
 private:
 
-    void registerController()
-    {
+    /* Модуль может использовать постоянное внешнее хранилище данных, содержимое которого
+     * хранится в произваольной форме. Запись и чтение происходит со всей информацией разом */
+    void loadData() {
+        if (_callback_func == nullptr) {
+            throw_exception("Callback function is null");
+        }
         nlohmann::json request;
         nlohmann::json response;
         request["module"] = _workname;
-        request["action"] = "ctrl_reg";
-        request["crtl_name"] = _workname;
+        request["action"] = "load";
+        _callback_func(request, response);
+        _data_buffer = response;
+    }
+
+    void saveData() {
+        if (_callback_func == nullptr) {
+            throw_exception("Callback function is null");
+        }
+        nlohmann::json request;
+        nlohmann::json response;
+        request["module"] = _workname;
+        request["action"] = "save";
+        request["data"] = _data_buffer;
+        _callback_func(request, response);
+    }
+
+    void registerController() {
+        if (_callback_func == nullptr) {
+            throw_exception("Callback function is null");
+        }
+        nlohmann::json request;
+        nlohmann::json response;
+        request["module"] = _workname;
+        request["action"] = "register";
         _callback_func(request, response);
     }
 
@@ -242,5 +262,6 @@ private:
     std::string         _json_dump_buffer;
     bool                _used;
     nlohmann::json      _stashed_objects;
+    nlohmann::json      _data_buffer;
 
 };
